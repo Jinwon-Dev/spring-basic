adb94722af9db9182a5dbd2e0833e357729f2497
Jinwon-Dev <yjw9424@naver.com>
2022-11-23 20:00:53 +0900

    AppConfig 리팩터링

--- src/main/java/basic/springbasic/AppConfig.java
+++ src/main/java/basic/springbasic/AppConfig.java
@@ -1,6 +1,8 @@
 package basic.springbasic;
 
+import basic.springbasic.discount.DiscountPolicy;
 import basic.springbasic.discount.FixDiscountPolicy;
+import basic.springbasic.member.MemberRepository;
 import basic.springbasic.member.MemberService;
 import basic.springbasic.member.MemberServiceImpl;
 import basic.springbasic.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import basic.springbasic.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
