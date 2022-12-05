ec8e3a8029762b6e53b7fba53f9fda2f02434c69
Jinwon-Dev <yjw9424@naver.com>
2022-12-05 16:11:58 +0900

    다양한 의존관계 주입 방법

--- src/main/java/basic/springbasic/order/OrderServiceImpl.java
+++ src/main/java/basic/springbasic/order/OrderServiceImpl.java
@@ -12,11 +12,30 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
+//    @Autowired private MemberRepository memberRepository; -> 필드 주입
+//    @Autowired private DiscountPolicy discountPolicy; -> 필드 주입
+
+//    @Autowired -> 수정자를 통한 주입(setter 주입)
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+//    @Autowired -> 수정자를 통한 주입(setter 주입)
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    @Autowired // 생성자가 한 개일 경우 생략 가능
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+//    @Autowired -> 일반 메서드를 통한 주입
+//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
