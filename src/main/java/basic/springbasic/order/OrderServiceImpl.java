4aea7ff6811b75c5383375fde23aac1b88e0dd6d
Jinwon-Dev <yjw9424@naver.com>
2022-12-06 13:20:57 +0900

    롬복 라이브러리 적용

--- src/main/java/basic/springbasic/order/OrderServiceImpl.java
+++ src/main/java/basic/springbasic/order/OrderServiceImpl.java
@@ -3,39 +3,15 @@ package basic.springbasic.order;
 import basic.springbasic.discount.DiscountPolicy;
 import basic.springbasic.member.Member;
 import basic.springbasic.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // final이 붙은 필드를 이용해 생성자를 만들어준다.
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-//    @Autowired private MemberRepository memberRepository; -> 필드 주입
-//    @Autowired private DiscountPolicy discountPolicy; -> 필드 주입
-
-//    @Autowired -> 수정자를 통한 주입(setter 주입)
-//    public void setMemberRepository(MemberRepository memberRepository) {
-//        this.memberRepository = memberRepository;
-//    }
-
-//    @Autowired -> 수정자를 통한 주입(setter 주입)
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-//        this.discountPolicy = discountPolicy;
-//    }
-
-    @Autowired // 생성자가 한 개일 경우 생략 가능
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
-//    @Autowired -> 일반 메서드를 통한 주입
-//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
